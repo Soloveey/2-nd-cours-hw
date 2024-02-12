d4acb47a38696d527b3bedaad07883ac3bfa8bba
тут имя пользователя <  >
2024-02-12 23:00:09 +0300

    Выполнил д.з. №7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Ига "Времена года"
 function seasonYears() {
     let numberMonth = prompt('Введите номер месяца');
    if (numberMonth == 1 || numberMonth == 2 || numberMonth == 12) {
@@ -16,4 +17,23 @@ function seasonYears() {
     alert (numberMonth);
   }
   }
+
+  // Игра "Угадай слово"
+  function guessTheWord(){
+    let fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    fruits = fruits.sort(() => Math.random() - 0.5);
+    alert(`Запомните первое и последнее слово: ${fruits}`);
+    
+    let firstFruits = prompt('Угадайте первое слово?').toLowerCase();
+    let lastFruits = prompt('Угадайте последнее слово?').toLowerCase();
+    if (firstFruits.toLowerCase() === fruits[0].toLowerCase() && lastFruits === fruits[fruits.length - 1].toLowerCase()) {
+      alert('Это правильный ответ!!!');
+    }
+    else if (firstFruits.toLowerCase() !== fruits[0].toLowerCase() && lastFruits !== fruits[fruits.length - 1].toLowerCase()) {
+      alert('Ответ неверный!');
+    }
+    else {
+      alert('Вы были близки к победе!');
+    }
+    }
   
